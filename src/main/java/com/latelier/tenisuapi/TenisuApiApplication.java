a1d85a25413e2b32e6bc2445b7e20b65370d7653
saqqaoui <siham.aqqaoui@gmail.com>
2025-05-19 08:11:09 +0200

    first commit

---
+++ src/main/java/com/latelier/tenisuapi/TenisuApiApplication.java
@@ -0,0 +1,13 @@
+package com.latelier.tenisuapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenisuApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TenisuApiApplication.class, args);
+    }
+
+}
